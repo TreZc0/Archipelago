166a7004d24e3d73b08adbb7e7fb6e93df9baaaa
stubob <mr.stubob@gmail.com>
2025-08-13 16:19:36 -0600

    fixed Progressive max links - Gloves Progressive max links - Boots

--- PathOfExileClient.py
+++ PathOfExileClient.py
@@ -9,5 +9,5 @@ import Utils
 
 if __name__ == "__main__":
     faulthandler.enable()
-    Utils.init_logging("poe", exception_logger="Client")
+    Utils.init_logging("pathofexile", exception_logger="Client")
     launch()
