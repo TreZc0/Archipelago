bd413d8e32dec06cd6a9a8f8e2043afbcaccf73a
aurabot <aurabot24@gmail.com>
2026-04-07 14:27:20 -0500

    Cleanup

--- Fill.py
+++ Fill.py
@@ -193,12 +193,13 @@ def fill_restrictive(multiworld: MultiWorld, base_state: CollectionState, locati
                     if spot_to_fill is None:
                         # Can't place this item, move on to the next
                         unplaced_items.append(item_to_place)
-                        logging.error(f"Failed to place {item_to_place} by swapping. Swap attempts: {swap_attempts}")
-                        logging.error(f"Current maximum state is {maximum_exploration_state}, {maximum_exploration_state.prog_items}")
-                        temp = multiworld.worlds[item_to_place.player].door_rando_world.get_region("Hyrule Castle Courtyard Northeast", 1)
-                        logging.error(f"Hyrule Castle Courtyard Northeast is_light_world, is_dark_world: {temp.is_light_world}, {temp.is_dark_world}")
-                        for region in multiworld.get_regions(item_to_place.player):
-                            logging.error(f"Can reach {region.name}: {region.can_reach(maximum_exploration_state)}")
+                        # Temp code for debugging
+                        # logging.error(f"Failed to place {item_to_place} by swapping. Swap attempts: {swap_attempts}")
+                        # logging.error(f"Current maximum state is {maximum_exploration_state}, {maximum_exploration_state.prog_items}")
+                        # temp = multiworld.worlds[item_to_place.player].door_rando_world.get_region("Hyrule Castle Courtyard Northeast", 1)
+                        # logging.error(f"Hyrule Castle Courtyard Northeast is_light_world, is_dark_world: {temp.is_light_world}, {temp.is_dark_world}")
+                        # for region in multiworld.get_regions(item_to_place.player):
+                        #     logging.error(f"Can reach {region.name}: {region.can_reach(maximum_exploration_state)}")
                         continue
                 else:
                     unplaced_items.append(item_to_place)
