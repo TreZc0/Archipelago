58dda3b79c6fa8a9bdf230f12f8eaeeb62343a4e
aurabot <aurabot24@gmail.com>
2026-06-13 15:07:16 -0500

    Fixed a bug where the HC BK was being left out of the itempool based on irrelevant doors settings.

--- worlds/alttpr/Items.py
+++ worlds/alttpr/Items.py
@@ -296,7 +296,7 @@ def create_all_items(world: ALttPRWorld) -> None:
     # If we're playing Standard mode with keysanity, we need to manually place the escape keys to prevent
     # getting BK'd in the escape sequence. This key is placed later in the pre_fill() stage of generation.
     dr_itempool = world.door_rando_world.itempool.copy()
-    if world.options.world_mode.value == "standard":
+    if world.options.world_mode.value == "standard" and world.options.door_shuffle.value == "vanilla":
         if world.options.small_key_shuffle.value:
             escape_keys = [item for item in dr_itempool if item.name == "Small Key (Escape)"]
             for key in escape_keys:
@@ -384,7 +384,7 @@ def place_pre_fill_items(world: ALttPRWorld) -> None:
         # If keysanity is enabled, the keys for the escape sequence should still be sphere 0,
         # to prevent the player from being near-instantly BK'd.
         if world.options.small_key_shuffle.value:
-            if world.options.door_shuffle.value or world.options.door_type_shuffle.value:
+            if world.options.door_shuffle.value != "vanilla":
                 world.options.local_items.value.add("Small Key (Escape)")
             elif world.options.key_drop_shuffle.value:
                 key_locations = ["Secret Passage", "Hyrule Castle - Map Chest", "Hyrule Castle - Map Guard Key Drop"]
@@ -407,7 +407,8 @@ def place_pre_fill_items(world: ALttPRWorld) -> None:
                 place_escape_key(small_key_locations, world, "Small")
 
         if world.options.big_key_shuffle.value and world.options.key_drop_shuffle.value:
-            if world.options.door_shuffle.value or world.options.door_type_shuffle.value:
+            if world.options.door_shuffle.value != "vanilla":
+                # No clue what the valid locations would be, good luck AP
                 world.options.local_items.value.add("Big Key (Escape)")
             else:
                 big_key_locations = ["Secret Passage", "Hyrule Castle - Map Chest", "Hyrule Castle - Map Guard Key Drop",
